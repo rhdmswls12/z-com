ce8871ebd9971e4f39c2e173f0f395399f5421bb
rhdmswls12 <rhgusdnr1293@naver.com>
2024-03-03 16:48:26 +0900

    프로젝트 생성 및 폴더 구조, 라우트 그룹 생성

---
+++ src/app/(afterLogin)/home/page.tsx
@@ -0,0 +1,9 @@
+import styles from '@/app/page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      홈페이지
+    </main>
+  )
+}
---
+++ src/app/(afterLogin)/layout.tsx
@@ -0,0 +1,8 @@
+export default function AfterLoginLayout({children}) {
+  return (
+    <div>
+      로그인 후 화면
+      {children}
+    </div>
+  )
+}
